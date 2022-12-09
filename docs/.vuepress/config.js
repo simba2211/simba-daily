fd747dc5d8b29fc73c33c5618ddd32ca3a775161
simba2211 <118442072+simba2211@users.noreply.github.com>
2022-12-09 10:29:34 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,3 +15,20 @@ module.exports = {
   plugins,
   themeConfig,
 }
+// vssue 评论插件
+  plugins: [
+    [
+      "vuepress-plugin-vssue-global",
+      {
+        platform: "github",
+        title: "[Comment]<%- frontmatter.title %>",
+        needComments: true,
+        // 其他的 Vssue 配置
+        autoCreateIssue: true,
+        clientId: "d3ec4ca6363950ca41a2",
+        clientSecret: "897465b6393f1d663e6128d2fab6959a0c0333cc",
+        owner: "lql95",
+        repo: "lql-notes",
+      },
+    ],
+  ],
